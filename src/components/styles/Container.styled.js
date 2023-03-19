433b86849d67ea3d21a43abb8ae0c674a631f58b
dmytro kuzmin <dmytrokuzmin@ukr.net>
2023-03-19 19:22:22 +0200

    created app, added basic deps and components

---
+++ src/components/styles/Container.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+  width: 70vw;
+  max-width: 100%;
+  padding: 0 20px;
+  margin: 0 auto;
+  background-color:  #bab8b8;
+`
