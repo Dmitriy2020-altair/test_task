a7525deadcd9e9d133f86bd9f7b73791dd8d5c4c
dmytro kuzmin <dmytrokuzmin@ukr.net>
2023-03-20 09:51:27 +0200

    removed hook

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteUser, getUsers } from '../store/actions/userActions';
 import { Button } from './styles/Button.styled';
