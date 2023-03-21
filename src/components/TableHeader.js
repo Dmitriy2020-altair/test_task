ae9fd04f4217d65a5d1be121f7c896d80ccc0d56
dmytro kuzmin <dmytrokuzmin@ukr.net>
2023-03-21 12:05:03 +0200

    added description for sorting

--- src/components/TableHeader.js
+++ src/components/TableHeader.js
@@ -1,12 +1,16 @@
 import { StyledTableHeader } from "./styles/StyledTableHeader.styled";
 
 export default function TableHeader() {
-  return(
-    <StyledTableHeader>
-      <div>id</div>
-      <div>Name</div>
-      <div>Age</div>
-      <div>About Person</div>
-    </StyledTableHeader>
+  return (
+    <>
+      <h3 style={{textAlign: 'center'}}>click on any value in the table to sort</h3>
+      <StyledTableHeader>
+        <div>id</div>
+        <div>Name</div>
+        <div>Age</div>
+        <div>About Person</div>
+      </StyledTableHeader>
+
+    </>
   )
 }
